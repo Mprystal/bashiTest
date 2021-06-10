edc4e178819495310f9cce34f9511e0ef1f9fc3e
Mprystal <mjprystal@gmail.com>
2021-06-10 15:01:54 -0400

    testing building

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('/build'));
-app.get('*', (req, res) =>
-  res.sendFile(path.resolve(__dirname, 'build', 'index.html')),
-);
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('build'));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html')),
+  );
+}
 
 app.listen(PORT, () => console.log(`App listening at port ${PORT}`));
 // app.listen(port, () => console.log(`listening on - http://localhost:${port}`));
